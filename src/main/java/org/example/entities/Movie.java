927755d81c757182b75f8ca9aee09fece666326e
ScriptCodes <143606581+ScriptCodes@users.noreply.github.com>
2024-09-18 12:08:27 +0200

    Movie Entity

--- src/main/java/org/example/entities/Movie.java
+++ src/main/java/org/example/entities/Movie.java
@@ -3,65 +3,42 @@ package org.example.entities;
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.Getter;
-import org.example.dtos.MovieDTO;
 
 import java.util.Set;
 
-/**
- * @author Daniel Rouvillain, Kevin
- *
- */
-
-
-
 @Data
 @Entity
-
+@Table(name = "movies")
 public class Movie {
     @Id
     @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
     private int id;
+
     @Column(name = "title", nullable = false)
     private String title;
+
     @Column(name = "overview", nullable = false)
     private String overview;
+
     @Column(name = "lower_rating", nullable = false)
     private double lowerRating;
+
     @Column(name = "upper_rating", nullable = false)
     private double upperRating;
 
-
-    @JoinColumn(name = "genre_id", nullable = false)
-
-    @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @ManyToMany
     @JoinTable(
             name = "movie_genre",
             joinColumns = @JoinColumn(name = "movie_id"),
             inverseJoinColumns = @JoinColumn(name = "genre_id")
     )
     private Set<Genre> genres;
-
     public Movie() {
     }
-
     public Movie(String title, String overview, double lowerRating, double upperRating) {
         this.title = title;
         this.overview = overview;
         this.lowerRating = lowerRating;
         this.upperRating = upperRating;
     }
-
-    public Movie(MovieDTO movieDTO) {
-        this.title = movieDTO.getTitle();
-        this.overview = movieDTO.getOverview();
-        this.lowerRating = movieDTO.getLowerRating();
-        this.upperRating = movieDTO.getUpperRating();
-    }
-    public Movie(Movie movie) {
-        this.title = movie.getTitle();
-        this.overview = movie.getOverview();
-        this.lowerRating = movie.getLowerRating();
-        this.upperRating = movie.getUpperRating();
-    }
-
 }
